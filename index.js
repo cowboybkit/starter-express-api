9cfcfe814a2ddc13461e09c76a0fb08091e05869
DragonWar.IO <hi@dragonwar.io>
2022-11-09 13:59:57 +0700

    add new

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const cloudflareScraper =  require('cloudflare-scraper');
+
 const app = express()
+app.use(bodyParser.json());
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.post('/scraper', async (req, res) => {
+    const destUrl = req.body.destUrl
+    const response = await cloudflareScraper.get(destUrl);
+    res.send(response)
+})
+
 app.listen(process.env.PORT || 3000)
